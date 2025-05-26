ab63473b93fbe5946bd96829ce9603b66818ec5a
OrianSH <orians@affilomania.com>
2025-05-26 10:03:28 +0300

    remove old Test

--- __tests__/purchase/purchaseTests.spec.js
+++ __tests__/purchase/purchaseTests.spec.js
@@ -18,19 +18,7 @@ test.describe('Purchase Tests', () => {
         await page.goto(utils.urlEnv);
         purchase = new Purchase(page);
     });
-    // Future Test for Purchase process
-    test('Taboo > LoggedIn Customer > Click on Purchase Button', async ({ page }) => {
-        const purchaseGoldButton = page.locator(utils.purchaseGoldButton);
-        await purchaseGoldButton.click();
-        await generalFunctions.clickOnXButton(page);
-        const toggleGoldButton = page.locator(utils.toggleGoldButton);
-        await toggleGoldButton.click();
-        const purchaseSecretButton = page.locator(utils.purchaseSecretButton);
-        await purchaseSecretButton.click();
-    });
-
     test('TEST > Taboo > Logged In Customer > Click on Purchase Button TEST', async ({ page }) => {
-        // await page.pause();
         await purchase.clickOnPurchaseGoldButton();
         await purchase.clickOnXButton()
         await purchase.toggleGoldButton();
